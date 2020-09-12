3993424c8a28f37cd4adb36c87b883c01c41b02b
abhishekD97 <abhishekdolli@gmail.com>
2020-09-13 01:53:20 +0530

    Add card section, skills section, and footer section and initialize EJS to document

---
+++ app.js
@@ -0,0 +1,16 @@
+for (var i = 0; i < document.querySelectorAll(".fab").length; i++) {
+  document.querySelectorAll(".fab")[i].addEventListener("click", function() {
+    switch (this.id) {
+      case 'git':
+        window.location.href = "https://github.com/abhishekD97"
+        break;
+      case 'lin':
+        window.location.href = "https://www.linkedin.com/in/abhishek-dolli-0526b4196"
+        break;
+      case 'ins':
+        window.location.href = "https://www.instagram.com/cyborg_8274/"
+        break;
+      default:
+    }
+  });
+}
