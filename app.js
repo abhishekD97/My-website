c39912d5dcd48371021bb80742ef92f322fcb236
abhishekD97 <abhishekdolli@gmail.com>
2020-09-16 16:02:53 +0530

    first attempt at contact page styling

--- app.js
+++ app.js
@@ -11,8 +11,18 @@ app.set('view engine', 'ejs');
 app.get("/",function(req,res){
   res.render("home");
 })
-app.get("/contact",function(req,res){
+app.get("/:loc",function(req,res){
+  let a = req.params.loc;
+  if (a==="home") {
+  res.render("home");
+  }else if (a==="education") {
+  res.render("education");
+  }else if(a==="contact"){
   res.render("contact");
+}else{
+  res.render("home")
+}
+
 })
 app.listen(3000,function(req,res){
   console.log("server up at 3000 local");
