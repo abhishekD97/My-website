8c3dd33daf2e3cc2ecacb5f7384a572785f87d77
abhishekD97 <abhishekdolli@gmail.com>
2020-09-17 03:07:18 +0530

    successfully added weather page and interconnected to weatherAPI

--- app.js
+++ app.js
@@ -19,9 +19,30 @@ app.get("/:loc",function(req,res){
   res.render("education");
   }else if(a==="contact"){
   res.render("contact");
-}else{
+}else if (a==="weatherWithout") {
+    res.render("weatherWithout");
+  }
+else{
   res.render("home")
-}
+}})
+
+app.post("/weather",function(req,response)
+{
+  const cityName = req.body.city
+  const APPID = "0de42b4ffa1db3bd38a07b88d3bac75c";
+  const uri = "https://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&APPID="+APPID+"&units=metric";
+  https.get(uri,function(res){
+    res.on("data",function(data){
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.list[0].main.temp;
+      const icon = weatherData.list[0].weather[0].icon;
+      const desc = weatherData.list[0].weather[0].description;
+      const cloudy = weatherData.list[0].clouds.all;
+      const humidity = weatherData.list[0].main.humidity;
+      const wind = weatherData.list[0].wind.speed;
+      const weatherIcon = "http://openweathermap.org/img/wn/"+icon+"@2x.png"
+      response.render('weather',{temp:temp,weatherIcon:weatherIcon,desc:desc,cloudy:cloudy,humidity:humidity,wind:wind});
+})})
 
 })
 app.listen(3000,function(req,res){
