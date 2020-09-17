e02f4030f9cc387be5852b9e12d0c7f8169aa672
abhishekD97 <abhishekdolli@gmail.com>
2020-09-17 20:20:12 +0530

    added error handling function to weather page

--- app.js
+++ app.js
@@ -4,47 +4,85 @@ const bodyParser = require("body-parser");
 const https = require("https");
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
-app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-
-app.get("/",function(req,res){
-  res.render("home");
-})
-app.get("/:loc",function(req,res){
-  let a = req.params.loc;
-  if (a==="home") {
-  res.render("home");
-  }else if (a==="education") {
-  res.render("education");
-  }else if(a==="contact"){
-  res.render("contact");
-}else if (a==="weatherWithout") {
-    res.render("weatherWithout");
-  }
-else{
-  res.render("home")
-}})
-
-app.post("/weather",function(req,response)
+function readTextFile(file)
 {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+
+
+app.set('view engine', 'ejs');
+app.get("/", function(req, res) {
+  res.render("home");
+})
+app.get("/:loc", function(req, res) {
+  let a = req.params.loc;
+  if (a === "home") {
+    res.render("home");
+  } else if (a === "education") {
+    res.render("education");
+  } else if (a === "contact") {
+    res.render("contact");
+  } else if (a === "weatherWithout") {
+    res.render("weatherWithout");
+  } else {
+    res.render("home")
+  }
+})
+
+app.post("/weather", function(req, response) {
   const cityName = req.body.city
-  const APPID = "0de42b4ffa1db3bd38a07b88d3bac75c";
-  const uri = "https://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&APPID="+APPID+"&units=metric";
-  https.get(uri,function(res){
-    res.on("data",function(data){
-      const weatherData = JSON.parse(data);
-      const temp = weatherData.list[0].main.temp;
-      const icon = weatherData.list[0].weather[0].icon;
-      const desc = weatherData.list[0].weather[0].description;
-      const cloudy = weatherData.list[0].clouds.all;
-      const humidity = weatherData.list[0].main.humidity;
-      const wind = weatherData.list[0].wind.speed;
-      const weatherIcon = "http://openweathermap.org/img/wn/"+icon+"@2x.png"
-      response.render('weather',{temp:temp,weatherIcon:weatherIcon,desc:desc,cloudy:cloudy,humidity:humidity,wind:wind});
-})})
+  const APPID = "0de42b4ffa1db3bd38a07b88d3bac75c"
+  const uri = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&APPID=" + APPID + "&units=metric";
+
+  https.get(uri, function(res) {
+    res.on("data", function(data) {
+      try {
+        const weatherData = JSON.parse(data);
+        const temp = weatherData.list[0].main.temp;
+        const icon = weatherData.list[0].weather[0].icon;
+        const desc = weatherData.list[0].weather[0].description;
+        const cloudy = weatherData.list[0].clouds.all;
+        const humidity = weatherData.list[0].main.humidity;
+        const wind = weatherData.list[0].wind.speed;
+        const weatherIcon = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+
+        response.render('weather', {
+          temp: temp,
+          weatherIcon: weatherIcon,
+          desc: desc,
+          cloudy: cloudy,
+          humidity: humidity,
+          wind: wind
+        })
+      } catch (err) {
+        console.log("error" + err.stack)
+      } finally {
+        console.log("good to go")
+      }
+
+    })
+  })
 
 })
-app.listen(3000,function(req,res){
+app.listen(process.env.PORT || 3000, function(req, res) {
   console.log("server up at 3000 local");
 })
